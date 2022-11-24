b5a2bf2e7b2dbc145fdc53f39a8ecb91f3d4f019
arkper <arkper@gmail.com>
2022-11-23 22:08:41 -0500

    changed Provider domain class

--- src/main/java/com/modern/office/scheduler/domain/Provider.java
+++ src/main/java/com/modern/office/scheduler/domain/Provider.java
@@ -14,12 +14,21 @@ public class Provider {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "provider_no")
 	private int providerNo;
+
 	@Column(name = "provider_first_name")
 	private String providerFirstName;
+	
 	@Column(name = "provider_last_name")
 	private String providerLastName;
+	
 	@Column(name = "provider_middle_initial")
 	private String providerMiddleInitial;
+
+	@Column(name = "is_provider")
+	private int isProvider;
+
+	@Column(name = "provider_active")
+	private boolean providerActive;
 
 	public int getProviderNo() {
 		return providerNo;
@@ -44,5 +53,17 @@ public class Provider {
 	}
 	public void setProviderMiddleInitial(String providerMiddleInitial) {
 		this.providerMiddleInitial = providerMiddleInitial;
+	}
+	public int getIsProvider() {
+		return isProvider;
+	}
+	public void setIsProvider(int isProvider) {
+		this.isProvider = isProvider;
+	}
+	public boolean isProviderActive() {
+		return providerActive;
+	}
+	public void setProviderActive(boolean providerActive) {
+		this.providerActive = providerActive;
 	}
 }
