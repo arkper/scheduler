02c116b80defe4c5f8e4f9a86c51d87ab8ebfa1d
arkper <arkper@gmail.com>
2022-11-24 21:05:16 -0500

    fixed security

--- src/main/java/com/modern/office/scheduler/CustomIpAuthenticationProvider.java
+++ src/main/java/com/modern/office/scheduler/CustomIpAuthenticationProvider.java
@@ -1,10 +1,10 @@
 package com.modern.office.scheduler;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,37 +21,47 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CustomIpAuthenticationProvider implements AuthenticationProvider {
     
-   Set<String> whitelist = new HashSet<String>();
-
-    public CustomIpAuthenticationProvider() {
-        whitelist.add("18.189.210.54");
-        whitelist.add("185.183.92.88");
-        whitelist.add("67.81.99.20");
-        whitelist.add("127.0.0.1");
-        whitelist.add("0:0:0:0:0:0:0:1");
-    }
+    private final Set<String> whitelist;
+	
+	private final String userId;
+	
+	private final String pwd;
+	
+	public CustomIpAuthenticationProvider(
+			@Value("${scheduler.security.whitelist}") Set<String> whitelist,
+			@Value("${scheduler.security.user}") String userId,
+			@Value("${scheduler.security.pwd}") String pwd)
+	{
+		this.whitelist = whitelist;
+		this.userId = userId;
+		this.pwd = pwd;
+	}
 
     public Authentication authenticate(Authentication auth) throws AuthenticationException {
         WebAuthenticationDetails details = (WebAuthenticationDetails) auth.getDetails();
         String userIp = details.getRemoteAddress();
         log.info("Remote IP address: " + userIp);
-        if(! whitelist.contains(userIp)){
+        if (!whitelist.contains(userIp))
+        {
             throw new BadCredentialsException("Invalid IP Address");
         }
         final String name = auth.getName();
-        final String password = auth.getCredentials().toString();
+        final String secret = auth.getCredentials().toString();
         
-        if (name.equals("user123") && password.equals("password123")) {
-        List<GrantedAuthority> authorities =new ArrayList<GrantedAuthority>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-        return new UsernamePasswordAuthenticationToken(name, password, authorities);
+        if (name.equals(userId) && secret.equals(pwd)) 
+        {
+            final List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+            return new UsernamePasswordAuthenticationToken(name, secret, authorities);
         }
-        else{
+        else
+        {
             throw new BadCredentialsException("Invalid username or password");
         }
     }
 
-    public boolean supports(Class<?> authentication) {
+    public boolean supports(Class<?> authentication) 
+    {
         return authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
--- src/main/java/com/modern/office/scheduler/SchedulerApplication.java
+++ src/main/java/com/modern/office/scheduler/SchedulerApplication.java
@@ -1,26 +1,17 @@
 package com.modern.office.scheduler;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.naming.AuthenticationException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.userdetails.User;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -28,55 +19,49 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-@SpringBootApplication(scanBasePackages = {"com.modern.office.scheduler"})
+@SpringBootApplication(scanBasePackages = { "com.modern.office.scheduler" })
 @EnableJpaRepositories
+@EnableConfigurationProperties
 public class SchedulerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulerApplication.class, args);
 	}
-	
+
 	@Bean
-	public ObjectMapper objectMapper()
-	{
+	public ObjectMapper objectMapper() {
 		ObjectMapper mapper = new ObjectMapper();
-		
+
 		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		
-	    mapper.setSerializationInclusion(Include.NON_NULL);
 
-	    // Write times as a String instead of a Long so its human readable.
-	    mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-	    mapper.registerModule(new JavaTimeModule());
+		mapper.setSerializationInclusion(Include.NON_NULL);
 
-	    return mapper;
+		// Write times as a String instead of a Long so its human readable.
+		mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		mapper.registerModule(new JavaTimeModule());
+
+		return mapper;
 	}
-	
-	@SuppressWarnings("deprecation")
+
 	@Configuration
-	@EnableWebSecurity//(debug = true)
-	public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@EnableWebSecurity
+	public class SecurityConfig {
 
-	    @Autowired
-	    private CustomIpAuthenticationProvider authenticationProvider;
+		@Autowired
+		private CustomIpAuthenticationProvider authenticationProvider;
 
-	    @Override
-	    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-	       //auth.inMemoryAuthentication().withUser("john").password("{noop}123").authorities("ROLE_USER");
-	       auth.authenticationProvider(authenticationProvider);
-	    }
+		@Bean
+		public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+			AuthenticationManagerBuilder authenticationManagerBuilder = http
+					.getSharedObject(AuthenticationManagerBuilder.class);
+			authenticationManagerBuilder.authenticationProvider(authenticationProvider);
+			return authenticationManagerBuilder.build();
+		}
 
-	    @Override
-	    protected void configure(final HttpSecurity http) throws Exception {
-	        // @formatter:off
-	        http.authorizeRequests()
-	            .antMatchers("/login").permitAll()
-//	            .antMatchers("/foos/**").hasIpAddress("11.11.11.11")
-	            .antMatchers("/foos/**").access("isAuthenticated() and hasIpAddress('11.11.11.11')")
-	            .anyRequest().authenticated()
-	            .and().formLogin().permitAll()
-	            .and().csrf().disable();
-	        // @formatter:on
-	    }
-    }
+		@Bean
+		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+			return http.build();
+		}
+	}
 }
