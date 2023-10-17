4ce82e335f9dd868c0bbf2694abfd6d90c5e0e5e
Arkady Perepelyuk <arkper@gmail.com>
2023-10-17 19:29:15 -0400

    Added message log. Reformatted appointment time

--- src/main/java/com/modern/office/sns/SnsService.java
+++ src/main/java/com/modern/office/sns/SnsService.java
@@ -1,5 +1,20 @@
 package com.modern.office.sns;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.modern.office.scheduler.AppConfig;
+import com.modern.office.scheduler.domain.Appointment;
+import com.modern.office.scheduler.domain.Business;
+import com.modern.office.scheduler.services.SchedulerApiService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import software.amazon.awssdk.http.SdkHttpResponse;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.*;
+
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
@@ -12,26 +27,6 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.StreamSupport;
-
-import com.modern.office.scheduler.domain.Patient;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.modern.office.scheduler.AppConfig;
-import com.modern.office.scheduler.domain.Appointment;
-import com.modern.office.scheduler.domain.Business;
-import com.modern.office.scheduler.services.SchedulerApiService;
-
-import lombok.extern.slf4j.Slf4j;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.http.SdkHttpResponse;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sns.SnsClient;
-import software.amazon.awssdk.services.sns.model.*;
 
 @Service
 @Slf4j
@@ -53,10 +48,7 @@ public class SnsService {
     private final ObjectMapper objectMapper;
     private final AppConfig appConfig;
 
-    public SnsService(final AppConfig appConfig,
-                      final SchedulerApiService schedulerApiService,
-                      final ObjectMapper objectMapper,
-                      final SnsClient snsClient) {
+    public SnsService(final AppConfig appConfig, final SchedulerApiService schedulerApiService, final ObjectMapper objectMapper, final SnsClient snsClient) {
         this.topicIncoming = appConfig.getTopicIncoming();
         log.info("Incoming SNS topic {}", this.topicIncoming);
 
@@ -67,19 +59,20 @@ public class SnsService {
         this.snsClient = snsClient;
     }
 
+    protected static boolean matchPhone(String apptPhone, String replyPhone) {
+        return replyPhone.replaceAll("[^0-9]", "").contains(apptPhone.replaceAll("[^0-9]", ""));
+    }
+
     @Scheduled(cron = "0 0 10 * * *", zone = "America/New_York")
     public void processNotifications() {
         log.info("Starting notification processing job");
-        StreamSupport.stream(this.schedulerApiService.getAppointmentToConfirm(0, 0, 0).spliterator(), false)
-                .filter(appt -> appt.getApptPhone() != null)
-                .forEach(appt -> this.processNotification(getNotificationMessage(appt), appt));
+        StreamSupport.stream(this.schedulerApiService.getAppointmentToConfirm(0, 0, 0).spliterator(), false).filter(appt -> appt.getApptPhone() != null).forEach(appt -> this.processNotification(getNotificationMessage(appt), appt));
         log.info("Finished notification processing job");
     }
 
     public void processNotification(String message, Appointment appt) {
-        if (Objects.isNull(appt.getApptPhone()) || appt.getApptPhone().length() < 10)
-        {
-            log.error ("Appointment {} has invalid phone - skipping", appt.getApptNo());
+        if (Objects.isNull(appt.getApptPhone()) || appt.getApptPhone().length() < 10) {
+            log.error("Appointment {} has invalid phone - skipping", appt.getApptNo());
             return;
         }
         var phone = this.transform(appt.getApptPhone());
@@ -94,15 +87,13 @@ public class SnsService {
     }
 
     public SdkHttpResponse optInPhone(final String phoneNumber) {
-        OptInPhoneNumberResponse response =
-                this.snsClient.optInPhoneNumber(OptInPhoneNumberRequest.builder().phoneNumber(phoneNumber).build());
+        OptInPhoneNumberResponse response = this.snsClient.optInPhoneNumber(OptInPhoneNumberRequest.builder().phoneNumber(phoneNumber).build());
 
         return response.sdkHttpResponse();
     }
 
     public boolean isPhoneOptedOut(final String phoneNumber) {
-        var response = this.snsClient.checkIfPhoneNumberIsOptedOut
-                (CheckIfPhoneNumberIsOptedOutRequest.builder().phoneNumber(phoneNumber).build());
+        var response = this.snsClient.checkIfPhoneNumberIsOptedOut(CheckIfPhoneNumberIsOptedOutRequest.builder().phoneNumber(phoneNumber).build());
 
         return response.sdkHttpResponse().isSuccessful() && response.isOptedOut();
     }
@@ -118,12 +109,9 @@ public class SnsService {
                 return "Opted Out";
             }
 
-            log.info("Sending notification message to {}", transformed);
+            log.info("Sending notification message {} to {}", message, transformed);
 
-            PublishRequest request = PublishRequest.builder()
-                    .message(message)
-                    .phoneNumber(transformed)
-                    .build();
+            PublishRequest request = PublishRequest.builder().message(message).phoneNumber(transformed).build();
 
             PublishResponse result = this.snsClient.publish(request);
             log.info(result.messageId() + " Message sent. Status is " + result.sdkHttpResponse().statusCode());
@@ -137,12 +125,7 @@ public class SnsService {
     public String subscribe(final String url) {
 
         try {
-            SubscribeRequest request = SubscribeRequest.builder()
-                    .protocol("http")
-                    .endpoint(url)
-                    .returnSubscriptionArn(true)
-                    .topicArn(this.topicIncoming)
-                    .build();
+            SubscribeRequest request = SubscribeRequest.builder().protocol("http").endpoint(url).returnSubscriptionArn(true).topicArn(this.topicIncoming).build();
 
             SubscribeResponse result = this.snsClient.subscribe(request);
             log.info("Subscription ARN is " + result.subscriptionArn() + ". Status is " + result.sdkHttpResponse().statusCode());
@@ -155,8 +138,7 @@ public class SnsService {
     }
 
     public void processReply(final String replyMessage) throws JsonProcessingException {
-        @SuppressWarnings("rawtypes")
-        LinkedHashMap data = this.objectMapper.readValue(replyMessage, LinkedHashMap.class);
+        @SuppressWarnings("rawtypes") LinkedHashMap data = this.objectMapper.readValue(replyMessage, LinkedHashMap.class);
 
         if (data.containsKey("SubscribeURL")) {
             log.info("Please visit url {}", data.get("SubscribeURL"));
@@ -167,10 +149,7 @@ public class SnsService {
         final var phoneNumber = (String) data.get("originationNumber");
         log.info("Updating appointment for phone {} with reply {}", phoneNumber, message);
         if (this.phoneEnabled(phoneNumber)) {
-            StreamSupport.stream(this.schedulerApiService.getAppointmentToConfirm(0, 0, 1).spliterator(), false)
-                    .filter(a -> matchPhone(a.getApptPhone(), phoneNumber))
-                    .findAny()
-                    .ifPresent(appointment -> this.updateAppointment(appointment, message, phoneNumber));
+            StreamSupport.stream(this.schedulerApiService.getAppointmentToConfirm(0, 0, 1).spliterator(), false).filter(a -> matchPhone(a.getApptPhone(), phoneNumber)).findAny().ifPresent(appointment -> this.updateAppointment(appointment, message, phoneNumber));
         }
     }
 
@@ -190,10 +169,6 @@ public class SnsService {
 
         log.info("Phone {} is not enabled", phone);
         return false;
-    }
-
-    protected static boolean matchPhone(String apptPhone, String replyPhone) {
-        return replyPhone.replaceAll("[^0-9]", "").contains(apptPhone.replaceAll("[^0-9]", ""));
     }
 
     private void updateAppointment(Appointment appointment, String message, String phoneNumber) {
@@ -223,19 +198,9 @@ public class SnsService {
         var business = this.schedulerApiService.getBusiness(1);
 
         if ("EN".equals(this.getLang(appt))) {
-            return String.format(NOTIFICATION_MESSAGE,
-                    appt.getApptDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)),
-                    appt.getApptStartTime(),
-                    this.getProviderName(appt.getProviderNo()),
-                    this.appConfig.getOfficeName(),
-                    this.getAddress(business));
+            return String.format(NOTIFICATION_MESSAGE, appt.getApptDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)), StringUtils.substringBeforeLast(appt.getApptStartTime(), ":"), this.getProviderName(appt.getProviderNo()), this.appConfig.getOfficeName(), this.getAddress(business));
         } else {
-            return String.format(NOTIFICATION_MESSAGE_RU,
-                    this.getProviderName(appt.getProviderNo()),
-                    this.appConfig.getOfficeName(),
-                    appt.getApptStartTime(),
-                    appt.getApptDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)),
-                    this.getAddress(business));
+            return String.format(NOTIFICATION_MESSAGE_RU, this.getProviderName(appt.getProviderNo()), this.appConfig.getOfficeName(), StringUtils.substringBeforeLast(appt.getApptStartTime(), ":"), appt.getApptDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)), this.getAddress(business));
         }
     }
 
@@ -244,17 +209,12 @@ public class SnsService {
     }
 
     private String getProviderName(int providerNo) {
-        return this.schedulerApiService.getAllProviders().stream().filter(p -> p.getProviderNo() == providerNo).findFirst()
-                .map(p -> p.getProviderFirstName() + " " + p.getProviderLastName())
-                .orElse("");
+        return this.schedulerApiService.getAllProviders().stream().filter(p -> p.getProviderNo() == providerNo).findFirst().map(p -> p.getProviderFirstName() + " " + p.getProviderLastName()).orElse("");
     }
 
     private void blackListPhone(String phone) {
         try {
-            Files.write(
-                    Paths.get(this.appConfig.getBlackListLocation()),
-                    this.transform(phone + "\n").getBytes(),
-                    StandardOpenOption.APPEND);
+            Files.write(Paths.get(this.appConfig.getBlackListLocation()), this.transform(phone + "\n").getBytes(), StandardOpenOption.APPEND);
         } catch (IOException e) {
             log.error("Failed to blacklist phone {}", phone);
             throw new RuntimeException(e);
