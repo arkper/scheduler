ce7adb3d17365d24f0c052382dd7ad530e5727b5
arkper <arkper@gmail.com>
2023-11-24 20:56:18 -0500

    added cors support

--- src/main/java/com/modern/office/scheduler/SchedulerApplication.java
+++ src/main/java/com/modern/office/scheduler/SchedulerApplication.java
@@ -70,8 +70,7 @@ public class SchedulerApplication {
 		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 			http.authorizeRequests().antMatchers("/reply").permitAll();
 			
-			http
-			   .csrf().disable()
+			http.cors().and().csrf().disable()
 			   .authorizeRequests()
 			   .anyRequest().authenticated().and().httpBasic();
 			return http.build();
