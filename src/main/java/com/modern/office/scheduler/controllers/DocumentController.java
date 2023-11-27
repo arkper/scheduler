10a6aa5f531a40ebc8367e675c2741eeb15174f5
arkper <arkper@gmail.com>
2023-11-26 21:06:33 -0500

    added document api

--- src/main/java/com/modern/office/scheduler/controllers/DocumentController.java
+++ src/main/java/com/modern/office/scheduler/controllers/DocumentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController("/documents")
 @Slf4j
 @RequiredArgsConstructor
 public class DocumentController {
