576766463e526ec23d4b524d311ae62233caffef
arkper <arkper@gmail.com>
2023-12-21 21:31:29 -0500

    Added correspondence report

--- src/main/java/com/modern/office/forms/controllers/CorrespondenceReportController.java
+++ src/main/java/com/modern/office/forms/controllers/CorrespondenceReportController.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/correspondence")
+@RestController
+@RequestMapping("/correspondence")
 @Slf4j
 @RequiredArgsConstructor
 public class CorrespondenceReportController {
