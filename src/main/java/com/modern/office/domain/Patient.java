3933f4ddc4ab8b291c4e2547b65bdf1bba4253e6
arkper <arkper@gmail.com>
2023-12-23 14:16:07 -0500

    Bug fix

--- src/main/java/com/modern/office/domain/Patient.java
+++ src/main/java/com/modern/office/domain/Patient.java
@@ -3,17 +3,7 @@ package com.modern.office.domain;
 import java.time.LocalDate;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -28,7 +18,7 @@ public class Patient {
 	@Column(name="patient_no")
 	private int patientNo;
 	
-	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = false)
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	@JoinColumn(name = "address_no", referencedColumnName = "address_no")
 	private Address address;
 	
