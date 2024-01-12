7043b10e857cd58f7a148cb489b607e0318ce77a
Arkady Perepelyuk <arkper@gmail.com>
2024-01-11 20:37:25 -0500

    Default document sorting

--- src/main/java/com/modern/office/forms/services/DocumentService.java
+++ src/main/java/com/modern/office/forms/services/DocumentService.java
@@ -7,16 +7,28 @@ import com.modern.office.repository.HippaDocumentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.StreamSupport.stream;
+
 @Service
 @RequiredArgsConstructor
 public class DocumentService{
     private final DocumentRepository documentRepository;
     private final HippaDocumentRepository hippaDocumentRepository;
+
     public Iterable<Document> getEDocuments(int patientNo) {
-        return this.documentRepository.findDocumentsByPatientNo(patientNo);
+        return StreamSupport.stream(
+                this.documentRepository.findDocumentsByPatientNo(patientNo).spliterator(), false)
+                .sorted((d1, d2) -> LocalDate.parse(d1.getRecordedOn()).isAfter(LocalDate.parse(d2.getRecordedOn()))? 1 : 0).toList();
     }
 
     public Iterable<HippaDocument> getHippaDocuments(int patientNo) {
-        return this.hippaDocumentRepository.findHippaDocumentsByPatientNo(patientNo);
+        return StreamSupport.stream(
+            this.hippaDocumentRepository.findHippaDocumentsByPatientNo(patientNo).spliterator(), false)
+                .sorted((d1, d2) -> LocalDate.parse(d1.getRecordedOn()).isAfter(LocalDate.parse(d2.getRecordedOn()))? 1 : 0).toList();
     }
 }
