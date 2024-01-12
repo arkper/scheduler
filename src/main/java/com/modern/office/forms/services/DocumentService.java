f111aecd5997e49ccd498f82d3a99de7e18e66f5
Arkady Perepelyuk <arkper@gmail.com>
2024-01-11 20:46:46 -0500

    Default document sorting

--- src/main/java/com/modern/office/forms/services/DocumentService.java
+++ src/main/java/com/modern/office/forms/services/DocumentService.java
@@ -5,30 +5,24 @@ import com.modern.office.domain.HippaDocument;
 import com.modern.office.repository.DocumentRepository;
 import com.modern.office.repository.HippaDocumentRepository;
 import lombok.RequiredArgsConstructor;
+import org.eclipse.collections.impl.factory.Lists;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.Comparator;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
-
-import static java.util.stream.StreamSupport.stream;
 
 @Service
 @RequiredArgsConstructor
-public class DocumentService{
+public class DocumentService {
     private final DocumentRepository documentRepository;
     private final HippaDocumentRepository hippaDocumentRepository;
 
     public Iterable<Document> getEDocuments(int patientNo) {
-        return StreamSupport.stream(
-                this.documentRepository.findDocumentsByPatientNo(patientNo).spliterator(), false)
-                .sorted((d1, d2) -> LocalDate.parse(d1.getRecordedOn()).isAfter(LocalDate.parse(d2.getRecordedOn()))? 1 : 0).toList();
+        return Lists.mutable.withAll(
+                        this.documentRepository.findDocumentsByPatientNo(patientNo))
+                .sortThisBy(Document::getSysId).reverseThis();
     }
 
     public Iterable<HippaDocument> getHippaDocuments(int patientNo) {
-        return StreamSupport.stream(
-            this.hippaDocumentRepository.findHippaDocumentsByPatientNo(patientNo).spliterator(), false)
-                .sorted((d1, d2) -> LocalDate.parse(d1.getRecordedOn()).isAfter(LocalDate.parse(d2.getRecordedOn()))? 1 : 0).toList();
+        return Lists.mutable.withAll(
+                        this.hippaDocumentRepository.findHippaDocumentsByPatientNo(patientNo))
+                .sortThisBy(Document::getSysId).reverseThis();
     }
 }
