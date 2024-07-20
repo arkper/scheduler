02c06aeb33ebe77badd861d245f246ea0ed3b354
Arkady Perepelyuk <arkper@gmail.com>
2024-07-20 18:33:05 -0400

    new UI build

--- src/main/java/com/modern/office/repository/PaymentComissionRepository.java
+++ src/main/java/com/modern/office/repository/PaymentComissionRepository.java
@@ -12,5 +12,5 @@ public interface PaymentComissionRepository extends CrudRepository<PaymentComiss
     Iterable<PaymentComission> findByProviderAndPaymentDate(String provider, LocalDate fromDate, LocalDate toDate);
 
     @Query("select PaymentComission p where p.provider=:provider and p.insurance in (:insurances) and p.paymentDate between :fromDate and :toDate")
-    Iterable<PaymentComission> findByProviderInsuranceAndPaymentDate(String provider, List<String> insurance, LocalDate fromDate, LocalDate toDate);
+    Iterable<PaymentComission> findByProviderInsuranceAndPaymentDate(String provider, List<String> insurances, LocalDate fromDate, LocalDate toDate);
 }
