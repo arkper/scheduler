7be82908997eea0ea1bf533b82588ea73df07d92
Arkady Perepelyuk <arkper@gmail.com>
2024-08-15 21:34:28 -0400

    Changed exempt billing codes

--- src/main/java/com/modern/office/domain/PaymentComission.java
+++ src/main/java/com/modern/office/domain/PaymentComission.java
@@ -1,47 +1,42 @@
 package com.modern.office.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.apache.commons.lang3.StringUtils;
 
 import java.time.LocalDate;
 
 @Entity
-@Table(name="payment_comissions")
+@Table(name = "payment_comissions")
 @Data
 @Accessors(chain = true)
 public class PaymentComission {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    @Column(name = "id")
     int id;
 
-    @Column(name="payment_date")
+    @Column(name = "payment_date")
     LocalDate paymentDate;
 
-    @Column(name="payment_amount")
+    @Column(name = "payment_amount")
     double paymentAmount;
 
-    @Column(name="billing_code")
+    @Column(name = "billing_code")
     String billingCode;
 
-    @Column(name="insurance")
+    @Column(name = "insurance")
     String insurance;
 
-    @Column(name="provider")
+    @Column(name = "provider")
     String provider;
 
-    @Column(name="patient")
+    @Column(name = "patient")
     String patient;
 
     public double getCommissionedAmount() {
-        return this.getBillingCode().startsWith("92004") || this.getBillingCode().startsWith("92014")
+        return StringUtils.startsWithAny(this.getBillingCode(), "92004", "92014", "92002", "92012")
                 ? 0
                 : this.paymentAmount;
     }
